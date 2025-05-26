333caa6e98536bddaf16897436733c792c4f708b
Darshan B R <darshan@sigvitas.com>
2025-05-26 16:56:38 +0530

    text changed

--- backend/routes/employee.js
+++ backend/routes/employee.js
@@ -842,7 +842,7 @@ router.post("/upload-excel-dynamic", upload.single("excelFile"), async (req, res
       to: `dverma@sigvitas.com`,
       subject: 'Re: Daily Data Uploading',
       // text: `Hello Sir, /n/n${userId2} uploaded the data successfully on ${formattedDate} at ${formattedTime}. Total number of users: ${total}/n/nBest Regards,/nDarshan`,
-      text: `Hello Sir,\n\n${userId2} uploaded the data successfully on ${formattedDate} at ${formattedTime}. \nTotal number of users: ${total}\n\nBest Regards,\nDarshan`,
+      text: `Hello Sir,\n\n${userId2} uploaded the data successfully on ${formattedDate} at ${formattedTime}. \nTotal Attorneys in the Roster: ${total}\n\nBest Regards,\nDarshan`,
     };
   
     transporter.sendMail(mailOptions, (error, info) => {
